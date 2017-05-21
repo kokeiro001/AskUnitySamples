4ed26b4e378e54ddd01625cbc7b43c2db1e92d3e
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-05-21 17:16:48 +0900

    タッチされた場所に線形補完で移動するサンプルを追加した
    resolve https://teratail.com/questions/74788

---
+++ UnityProject/Assets/TouchTweenMoveQuestion/TouchMoveSample.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TouchTweenMoveQuestion
+{
+    public class TouchMoveSample : MonoBehaviour
+    {
+        float time;
+        float duration;
+        Vector3 startPosition;
+        Vector3 endPosition;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                float x = Input.mousePosition.x;
+                float y = Input.mousePosition.y;
+                Vector3 currentScreenPoint = new Vector3(x, y, 0);
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint);
+
+                Move(touchPosition, 0.2f);
+            }
+
+            if (time <= duration)
+            {
+                time += Time.deltaTime;
+                var x = Mathf.Lerp(startPosition.x, endPosition.x, time / duration);
+                var y = Mathf.Lerp(startPosition.y, endPosition.y, time / duration);
+                transform.position = new Vector3(x, y);
+            }
+
+            Vector3 s_position = Input.mousePosition;
+            Vector3 w_position = Camera.main.ScreenToWorldPoint(s_position);
+
+            Debug.Log(w_position);
+        }
+
+        void Move(Vector3 endPosition, float duration)
+        {
+            time = 0;
+            this.duration = duration;
+            startPosition = transform.position;
+            this.endPosition = endPosition;
+        }
+    }
+}
