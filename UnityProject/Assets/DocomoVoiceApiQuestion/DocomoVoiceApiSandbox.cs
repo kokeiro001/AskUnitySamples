65f286b9480a5a9dc65afead20e5c248fb7d1753
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-05-21 16:56:41 +0900

    Docomoの音声合成APIをUnityで利用するサンプルを追加した

---
+++ UnityProject/Assets/DocomoVoiceApiQuestion/DocomoVoiceApiSandbox.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace DocomoVoiceApiQuestion
+{
+    // resolve for https://teratail.com/questions/76058
+
+    public class DocomoVoiceApiSandbox : MonoBehaviour
+    {
+        static readonly string docomoApiKey = @"<your api key>";
+        static readonly string SaveSoundFileName = @"docomoApiSound.wav";
+
+        public AudioClipMaker audioClipMaker;
+        public AudioSource audioSource;
+
+        public void OnButtonClick()
+        {
+            StartCoroutine(DownloadCoroutine());
+        }
+
+        public void OnPlayButtonClick()
+        {
+            PlaySound();
+        }
+
+        IEnumerator DownloadCoroutine()
+        {
+            string url = "https://api.apigw.smt.docomo.ne.jp/aiTalk/v1/textToSpeech?APIKEY=" + docomoApiKey;
+
+            Dictionary<string, string> aiTalksParams = new Dictionary<string, string>();
+            aiTalksParams["pitch"] = "1";
+            aiTalksParams["range"] = "1";
+            aiTalksParams["rate"] = "1";
+            aiTalksParams["volume"] = "1.5";
+
+            var ssml = CreateSSML(aiTalksParams);
+            byte[] postData = System.Text.Encoding.UTF8.GetBytes(ssml);
+
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers["Content-Type"] = "application/ssml+xml";
+            headers["Accept"] = "audio/L16";
+            WWW www = new WWW(url, postData, headers);
+
+            yield return www;
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.LogError(www.error);
+                yield break;
+            }
+
+            uint fs = 16000; // 16K
+
+            WAVHDR wavHdr = new WAVHDR()
+            {
+                formatid = 0x0001, //PCM 非圧縮
+                channel = 1, // ch=1 モノラル
+                fs = fs,    //
+                bytespersec = fs * 2, // 16bit 16K
+                blocksize = 2, // ブロックサイズ (Byte/sample×チャンネル数)->→16ビットモノラルなので 2
+                bitspersample = 16, // サンプルあたりのビット数 (bit/sample)
+                size = 10 * fs * 2, // 波形データのバイト数
+            };
+            wavHdr.fileSize = wavHdr.size + (uint)Marshal.SizeOf(wavHdr); // 全体のバイト数
+
+            Debug.Log("SaveWaveFile");
+            SaveWaveFile(wavHdr, ConvertBytesEndian(www.bytes));
+        }
+
+
+        private void SaveWaveFile(WAVHDR hdr, byte[] databuf)
+        {
+            using (FileStream fs = new FileStream(SaveSoundFileName, FileMode.Create, FileAccess.Write))
+            using (BinaryWriter bWriter = new BinaryWriter(fs))
+            {
+                // ヘッダ書きだし
+                foreach (byte b in hdr.getByteArray())
+                {
+                    bWriter.Write(b);
+                }
+                // 波形書きだし
+                foreach (var data in databuf)
+                {
+                    bWriter.Write(data);
+                }
+                bWriter.Flush();
+            }
+        }
+
+        byte[] ConvertBytesEndian(byte[] bytes)
+        {
+            byte[] newBytes = new byte[bytes.Length];
+            for (int i = 0; i < bytes.Length; i += 2)
+            {
+                newBytes[i] = bytes[i + 1];
+                newBytes[i + 1] = bytes[i];
+            }
+            return newBytes;
+        }
+
+        protected string CreateSSML(Dictionary<string, string> parts)
+        {
+            return @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<speak version = ""1.1"">
+<voice name = ""nozomi"">
+のぞみです。おはようございます。
+</voice >
+<break time = ""1000ms"" />
+<voice name = ""seiji"">
+ せいじです。こんにちは。
+</voice>
+</speak>";
+        }
+
+        private void PlaySound()
+        {
+            byte[] buf = File.ReadAllBytes(SaveSoundFileName);
+
+            var wavFileInfo = new WavFileInfo();
+            wavFileInfo.Analyze(buf);
+
+            AudioSource source = audioSource;
+            source.clip = audioClipMaker.Create(
+                "making_clip",
+                buf,
+                wavFileInfo.TrueWavBufIdx,
+                wavFileInfo.BitPerSample,
+                wavFileInfo.TrueSamples,
+                wavFileInfo.Channels,
+                wavFileInfo.Frequency,
+                false
+            );
+            source.Play();
+        }
+    }
+}
---
+++ UnityProject/Assets/DocomoVoiceApiQuestion/WAVHDR.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DocomoVoiceApiQuestion
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public class WAVHDR
+    {
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 riff = 0x46464952; /* RIFF */
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 fileSize;
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 wave = 0x45564157; /* WAVE */
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 fmt = 0x20746D66; /* fmt  */
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 fmtbytes = 16;
+        [MarshalAs(UnmanagedType.I2)]
+        public UInt16 formatid;
+        [MarshalAs(UnmanagedType.I2)]
+        public UInt16 channel;
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 fs;
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 bytespersec;
+        [MarshalAs(UnmanagedType.I2)]
+        public UInt16 blocksize;
+        [MarshalAs(UnmanagedType.I2)]
+        public UInt16 bitspersample;
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 data = 0x61746164; /* data */
+        [MarshalAs(UnmanagedType.I4)]
+        public UInt32 size;
+
+        //convert the struct to byte array
+        public byte[] getByteArray()
+        {
+            int len = Marshal.SizeOf(this);
+            byte[] arr = new byte[len];
+            IntPtr ptr = Marshal.AllocHGlobal(len);
+            Marshal.StructureToPtr(this, ptr, true/*or false*/);
+            Marshal.Copy(ptr, arr, 0, len /*or arr.Length*/);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+    }
+
+}
