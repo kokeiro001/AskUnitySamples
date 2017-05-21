0aeba851119631a61731e8b91e62fc97451f4c1b
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-05-21 17:14:48 +0900

    線形補間による移動サンプルを追加した
    resolve https://teratail.com/questions/76862

---
+++ UnityProject/Assets/MoveLerpQuestion/SlideObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoveLerpQuestion
+{
+    public class SlideObject : MonoBehaviour
+    {
+        [SerializeField] float speed_ = 0.5f;
+
+        Vector3 moveStart_;
+        Vector3 moveEnd_;
+        float time_;
+
+        Player player;
+        public void Awake()
+        {
+            player = FindObjectOfType<Player>();
+        }
+
+        public void Update()
+        {
+            if (!player) return;
+            if (!player.GetHaveFlag()) return;
+
+            time_ += Time.deltaTime * speed_;
+
+            var tmp = Vector3.Lerp(moveStart_, moveEnd_, time_);
+            transform.position = new Vector3(tmp.x, transform.position.y, transform.position.z);
+
+            UpdateInput();
+        }
+
+        public void UpdateInput()
+        {
+            if (time_ < 1) return;
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                StartSlide(2);  // 2右に移動する
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                StartSlide(-2);  // 2左に移動する
+            }
+        }
+
+        private void StartSlide(float moveLength)
+        {
+            time_ = 0;
+            moveStart_ = transform.position;    // 移動開始時の座標を記憶しておく
+            moveEnd_ = moveStart_ + new Vector3(moveLength, 0, 0);  // 移動終了の座標を計算する
+        }
+    }
+}
