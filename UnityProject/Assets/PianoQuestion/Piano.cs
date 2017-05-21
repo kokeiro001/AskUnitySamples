54ccd617c46267c670d20e9996d93b49bc6e3d03
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-05-21 17:02:11 +0900

    resolve https://teratail.com/questions/77032

---
+++ UnityProject/Assets/PianoQuestion/Piano.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piano : MonoBehaviour
+{
+    // resolve for https://teratail.com/questions/77032
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D tapCollision2D = Physics2D.OverlapPoint(tapPoint);
+
+            // ヒットしたオブジェクトが自分自身かどうか確認する。
+            // collider2DのgameObjectには、ヒットした相手のgameObjectが格納されているため、
+            // これとPianoコンポーネントがアタッチされているGameObjectの比較を行えば良い。
+            if (tapCollision2D.gameObject == gameObject)
+            {
+                Debug.Log("Play My Sound!!! name=" + name);
+                //keySound.PlayOneShot(keySound.clip);
+            }
+        }
+    }
+}
